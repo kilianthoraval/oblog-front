375689ad0826b0ab8da37d3afca594a2c8c41443
kilianthoraval <kthoraval@gmail.com>
2022-11-04 11:44:43 +0100

    dynamisation

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 const router = require('./router.js');
 const express = require('express');
 const app = express();
+const articles = require('./data/articles.json')
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/integration');
 
 // - le dossier qui contient les fichiers statiques
 app.use(express.static(__dirname + '/static'));
+
+
+// app.use((request, response, next) => {
+//     app.locals.articlesList = articles;
+//     next();
+// })
+
 
 app.use(router);
 
--- router.js
+++ router.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const articles = require('./data/articles.json')
+
 
 // étape 2 : 1ere route + modif html
 
 router.get('/', (request, response) => {
-    response.render('index.ejs');
+    response.render('index.ejs',{
+        articles
+    });
 });
 
-
+// router.get('/article/:id', (request, response) => {    
+//     let requestedArticle;
+//     for (article of articles) {
+//         if (article.name === request.params.id) {
+//             requestedArticle = article;
+//             break;
+//         }
+//     }
+//     response.render('article.ejs',{
+//         requestedArticle
+//     })
+// });
 
 
 // j'exporte mon objet router
